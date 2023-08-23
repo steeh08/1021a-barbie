724254a73f1acd22d1879704c2f8527ee01a4876
steeh08 <stefanny.rodrigues@estudante.ifms.edu.br>
2023-08-23 07:05:00 -0400

    Atividade 22/08

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import Header from './componentes/header/Header'
 import './App.css'
-import Footer from './componentes/footer/Footer'
+import Filme from './componentes/filme/Filme'
 
 function App() {
   return (
     <div>
-    <Header/><Footer/>
+    <Header/><Filme/>
     </div>
   )
 }
---
+++ src/componentes/filme/Filme.tsx
@@ -0,0 +1,14 @@
+import Imagem from "./../assets/barbie.png"
+import './Filme.css'
+function Filme(){
+    return(
+        <div className="filme-content">
+            Filme
+            <h1>Barbie</h1>
+            <p className="sinopse">Depois de ser expulsa da Barbieland por ser 
+            uma boneca de aparência menos do que perfeita, Barbie 
+            parte para o mundo humano em busca da verdadeira felicidade.</p>
+        </div>
+    )
+}
+export default Filme
